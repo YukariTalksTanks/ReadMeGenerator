6633f691a2804cce5c2f9e37ca5f0ad7855f54c0
Preston Bradford <psbradford4100@att.net>
2020-07-04 13:38:45 -0400

    yeet

---
+++ index.js
@@ -0,0 +1,82 @@
+// array of questions for user
+var inquirer = require("inquirer");
+
+var genMark = require("./utils/generateMarkdown");
+
+const questions = [
+  {
+    type: "input",
+    message: "Enter project name: ",
+    name: "projectName"
+  },
+  {
+    type: "input",
+    message: "Enter username:",
+    name: "username"
+  },
+  {
+    type: "input",
+    message: "Enter Email:",
+    name: "email"
+  },
+  {
+    type: "input",
+    message: "Enter a description:",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "Enter installation instructions:",
+    name: "install"
+  },
+  {
+    type: "input",
+    message: "Enter usage info:",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "Enter contribution guidelines:",
+    name: "contribution"
+  },
+  {
+    type: "input",
+    message: "Enter test instructions:",
+    name: "tests"
+  },
+  {
+    type: 'list',
+    name: 'value',
+    message: 'Pick a license:',
+    choices: [
+      {
+        title: 'MIT', value: "mit"},
+      { title: 'GNU', value: 'gnu' },
+      { title: 'Apache', value: 'apache' }
+    ],
+    initial: 1
+  }
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+
+  var fs = require('fs');
+
+  fs.writeFile(fileName, data, function (err) {
+    if (err) throw err;
+    console.log('File written successfully.');
+  });
+}
+
+// function to initialize program
+function init() {
+  inquirer.prompt(questions)
+    .then(function (response) {
+      console.log(response);
+      writeToFile("TestMe.md", genMark(response));
+    })
+}
+
+// function call to initialize program
+init();
